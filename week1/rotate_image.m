9d530af55aaa820428cc10666c1f2f309c1e8bdd
Jorge Garcia <jagbolanos@gmail.com>
2014-01-23 16:13:35 -0600

    fixed center of rotation and image location

--- week1/rotate_image.m
+++ week1/rotate_image.m
@@ -5,9 +5,9 @@ function new_image = rotate_image(original_image, angle)
     max_side = max(width, height) / 2;
     for x=1:width
         for y=1:height
-            new_position = R * [(x-1 - max_side)/max_side;(y-1 - max_side)/max_side];
-            new_x = floor(new_position(1) * max_side + max_side + 1);
-            new_y = floor(new_position(2) * max_side + max_side + 1);
+            new_position = R * [(x-1 - width/2)/max_side;(y-1 - height/2)/max_side];
+            new_x = floor(new_position(1) * max_side + width/2 + 1);
+            new_y = floor(new_position(2) * max_side + height/2 + 1);
             if (new_x >= 1 && new_x <= width && new_y >= 1 && new_y <= height)
                 new_image(new_x, new_y) = original_image(x, y);
             end
